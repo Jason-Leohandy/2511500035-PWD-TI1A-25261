719ffafcfaf874f74ba20995385622ea7ba4ef88
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-10-28 12:41:30 +0700

    script.js

--- pertemuan-02/README.md
+++ pertemuan-02/README.md
@@ -5,4 +5,7 @@ Nama : Jason Leohandy
 Hari ini, Rabu 01 Oktober,2025 saya mempelajari:
 <ol>
  <li>git add,commit,git push origin main</li>
+ <li>browser akan melengkapi elemen atau tag yang hilang</li>
+ <li>struktur HTML5 yang lengkap dengan emmet</li>
+ 
  </ol>
--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,6 +1,18 @@
+document.querySelector("form").addEventListener("submit", function (e) { 
+  const nama = document.getElementById("txtNama").value.trim(); 
+  const email = document.getElementById("txtEmail").value.trim(); 
+  const pesan = document.getElementById("txtPesan").value.trim(); 
+ 
+  if (nama === "" || email === "" || pesan === "") { 
+    alert("Semua kolom wajib diisi!"); 
+    e.preventDefault(); 
+  } else { 
+    alert("Terima kasih, " + nama + "! Pesan Anda telah dikirim."); 
+  } 
+}); 
+
 document.getElementById("menuToggle").addEventListener("click", function () {
   document.querySelector("nav").classList.toggle("active");
-  console.log("pitak bencong");
 });
 
 document.querySelector("form").addEventListener("submit", function (e) {
