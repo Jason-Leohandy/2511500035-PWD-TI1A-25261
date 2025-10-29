4323cc7a4b82676b64e9f4df638c0195b2b2081f
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-10-29 12:21:41 +0700

    script.js

--- pertemuan-06/script.js
+++ pertemuan-06/script.js
@@ -1,16 +1,3 @@
-document.querySelector("form").addEventListener("submit", function (e) { 
-  const nama = document.getElementById("txtNama").value.trim(); 
-  const email = document.getElementById("txtEmail").value.trim(); 
-  const pesan = document.getElementById("txtPesan").value.trim(); 
- 
-  if (nama === "" || email === "" || pesan === "") { 
-    alert("Semua kolom wajib diisi!"); 
-    e.preventDefault(); 
-  } else { 
-    alert("Terima kasih, " + nama + "! Pesan Anda telah dikirim."); 
-  } 
-}); 
-
 document.getElementById("menutoggle").addEventListener("click", function () {
   const nav = document.querySelector("nav"); 
   nav.classList.toggle("active"); 
