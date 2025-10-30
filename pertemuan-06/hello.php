29eb4d2cc542b46c945d424f0baac8b27ed7db75
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-10-30 11:33:17 +0700

    hello.php

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -1,3 +1,9 @@
-<?php
-  echo "halo dunia!";
-?>
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <title>Belajar PHP Dasar</title> 
+</head> 
+<body> 
+  <h1><?php echo "Halo, Dunia PHP!"; ?></h1> 
+</body> 
+</html> 
