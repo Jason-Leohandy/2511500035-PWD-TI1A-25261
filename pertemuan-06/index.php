28c0cd9793f0c9545fee5930b922d245cbb07679
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-11-03 16:43:45 +0700

    index.php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -151,7 +151,7 @@ for ($i = 0; $i < count($namaMatkul); $i++) {
     if ($hadir[$i] < 70) {
         $grade = "E";
         $mutu = 0.00;
-    } elseif ($nilaiAkhir >= 55) {
+    } elseif ($nilaiAkhir >= 91 {
         $grade = "A";
         $mutu = 4.00;
     } elseif ($nilaiAkhir >= 80) {
