612ee8d981e0297d1bd5b2e941dd616268ab4ff1
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-11-03 16:23:23 +0700

    index.php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -122,10 +122,44 @@
 
 
         <section id="ipk">
-            
-
-
+            <h2>Nilai Saya</h2>
+            <?php
+            $namaMatkul1;
+            $namaMatkul2;
+            $namaMatkul3; 
+            $namaMatkul4; 
+            $namaMatkul5;
+            $sksMatkul1;
+            $sksMatkul2; 
+            $sksMatkul3; 
+            $sksMatkul4; 
+            $sksMatkul5;
+            $nilaiHadir1; 
+            $nilaiHadir2; 
+            $nilaiHadir3; 
+            $nilaiHadir4; 
+            $nilaiHadir5;
+            $nilaiTugas1;
+            $nilaiTugas2; 
+            $nilaiTugas3; 
+            $nilaiTugas4; 
+            $nilaiTugas5;
+            $nilaiUTS1;
+            $nilaiUTS2;
+            $nilaiUTS3; 
+            $nilaiUTS4; 
+            $nilaiUTS5;
+            $nilaiUAS1; 
+            $nilaiUAS2; 
+            $nilaiUAS3; 
+            $nilaiUAS4; 
+            $nilaiUAS;
+           ?>
        </section>
+
+       <h2>
+       </h2>
+
     </main>
     <footer>
         <p>&copy;2025 Jason Leohandy Nim [2511500035]</p>
