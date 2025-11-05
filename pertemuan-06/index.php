15b331a28c181ea0abb78ad79a0e238689645716
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-11-05 10:29:02 +0700

    index.php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -123,10 +123,8 @@
 
         <section id="ipk">
      <?php
-// =========================
-// DATA MATA KULIAH
-// =========================
-$namaMatkul = ["Algoritma dan Struktur Data", "Agama", "Matematika Diskrit", "Basis Data", "Pemrograman Web Dasar"];
+
+$namaMatkul = ["Kalkulus", "Logika Informatika", "Konsep Basis Data", "Wawasan Berbudi Luhur", "Pemrograman Web Dasar"];
 $sks        = [4, 2, 3, 3, 3];
 $hadir      = [90, 70, 80, 75, 69];
 $tugas      = [60, 50, 70, 80, 80];
@@ -140,18 +138,13 @@ echo "<section id='ipk'>";
 echo "<h2>Nilai Saya</h2>";
 
 for ($i = 0; $i < count($namaMatkul); $i++) {
-    // =========================
-    // Perhitungan Nilai Akhir
-    // =========================
+
     $nilaiAkhir = (0.1 * $hadir[$i]) + (0.2 * $tugas[$i]) + (0.3 * $uts[$i]) + (0.4 * $uas[$i]);
 
-    // =========================
-    // Menentukan Grade & Mutu
-    // =========================
     if ($hadir[$i] < 70) {
         $grade = "E";
         $mutu = 0.00;
-    } elseif ($nilaiAkhir >= 91 {
+    } elseif ($nilaiAkhir >= 90) {
         $grade = "A";
         $mutu = 4.00;
     } elseif ($nilaiAkhir >= 80) {
@@ -175,7 +168,7 @@ for ($i = 0; $i < count($namaMatkul); $i++) {
     } elseif ($nilaiAkhir >= 50) {
         $grade = "C-";
         $mutu = 1.70;
-    } elseif ($nilaiAkhir >= 40) {
+    } elseif ($nilaiAkhir >= 35) {
         $grade = "D";
         $mutu = 1.00;
     } else {
@@ -183,21 +176,12 @@ for ($i = 0; $i < count($namaMatkul); $i++) {
         $mutu = 0.00;
     }
 
-    // =========================
-    // Status Lulus / Gagal
-    // =========================
     $status = in_array($grade, ["A", "A-", "B+", "B", "B-", "C+", "C", "C-"]) ? "LULUS" : "GAGAL";
 
-    // =========================
-    // Bobot dan Akumulasi
-    // =========================
     $bobot = $mutu * $sks[$i];
     $totalBobot += $bobot;
     $totalSKS += $sks[$i];
 
-    // =========================
-    // OUTPUT SETIAP MATAKULIAH
-    // =========================
     echo "<h3>Nama Mata Kuliah ke-" . ($i + 1) . ": {$namaMatkul[$i]}</h3>";
     echo "SKS: {$sks[$i]}<br>";
     echo "Kehadiran: {$hadir[$i]}<br>";
@@ -211,9 +195,7 @@ for ($i = 0; $i < count($namaMatkul); $i++) {
     echo "Status: $status<br><hr>";
 }
 
-// =========================
-// HITUNG IPK
-// =========================
+
 $IPK = $totalBobot / $totalSKS;
 
 echo "<h3>Total Bobot = " . number_format($totalBobot, 2) . "</h3>";
@@ -222,9 +204,6 @@ echo "<h3>IPK = " . number_format($IPK, 2) . "</h3>";
 echo "</section>";
 ?>
        </section>
-
-       <h2>
-       </h2>
 
     </main>
     <footer>
