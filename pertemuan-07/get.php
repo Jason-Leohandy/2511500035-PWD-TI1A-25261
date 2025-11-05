02014c4b4e9d6b7059817bd25ab44f45f087534c
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2025-11-05 12:06:44 +0700

    get.php

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -3,7 +3,6 @@ session_start();
 $sesname = $_SESSION["nama"];
 $sesemail = $_SESSION["email"];
 $sespesan= $_SESSION["pesan"];
-echo "$sesname  $sesemail  $sespesan";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -89,6 +88,11 @@ echo "$sesname  $sesemail  $sespesan";
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
       </form>
+      <p>terimakasih sudah menghubungi kami :
+        <label>Nama : <strong><?php echo $sesname; ?></strong></label>
+        <label>Email : <strong><?php echo $sesemail; ?></strong></label>
+        <label>Pesan : <strong><?php echo $sespesan; ?></strong></label>
+</p>
     </section>
   </main>
 
