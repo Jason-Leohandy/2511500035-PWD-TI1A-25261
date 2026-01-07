84753a6af5501432afa6d40bf48fbadbf5864934
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2026-01-08 06:55:31 +0700

    main.document

--- pertemuan-15/koneksi.php
+++ pertemuan-15/koneksi.php
@@ -3,9 +3,11 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "db_pwd2025";
+$db2  = "Tryout_pwd2026"
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $db2);
 
 if (!$conn) {
   die("Koneksi gagal: " . mysqli_connect_error());
 }
+
